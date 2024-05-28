4d79db2bf02968e12d8434b3ffa0c37e71c7752c
lucifer <azl397985856@gmail.com>
2024-05-28 15:17:07 +0000

    gp

--- src/server/api/check-proof.ts
+++ src/server/api/check-proof.ts
@@ -12,24 +12,25 @@ import {createAuthToken, verifyToken} from "../utils/jwt";
  */
 export const checkProof: HttpResponseResolver = async ({request}) => {
   try {
-    const body = CheckProofRequest.parse(await request.json());
+    console.log('checkProof', request)
+    // const body = CheckProofRequest.parse(await request.json());
 
-    const client = TonApiService.create(body.network);
-    const service = new TonProofService();
+    // const client = TonApiService.create(body.network);
+    // const service = new TonProofService();
 
-    const isValid = await service.checkProof(body, (address) => client.getWalletPublicKey(address));
-    if (!isValid) {
-      return badRequest({error: 'Invalid proof'});
-    }
+    // const isValid = await service.checkProof(body, (address) => client.getWalletPublicKey(address));
+    // if (!isValid) {
+    //   return badRequest({error: 'Invalid proof'});
+    // }
 
-    const payloadToken = body.proof.payload;
-    if (!await verifyToken(payloadToken)) {
-      return badRequest({error: 'Invalid token'});
-    }
+    // const payloadToken = body.proof.payload;
+    // if (!await verifyToken(payloadToken)) {
+    //   return badRequest({error: 'Invalid token'});
+    // }
 
-    const token = await createAuthToken({address: body.address, network: body.network});
+    // const token = await createAuthToken({address: body.address, network: body.network});
 
-    return ok({token: token});
+    return ok({token: 'token'});
   } catch (e) {
     return badRequest({error: 'Invalid request', trace: e});
   }
