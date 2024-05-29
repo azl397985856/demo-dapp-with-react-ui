15540ac3e16ea31aed9a5a1c1798b758bba87b2d
lucifer <azl397985856@gmail.com>
2024-05-29 05:17:11 +0000

    publish

--- src/App.tsx
+++ src/App.tsx
@@ -138,7 +138,7 @@ const tonkeeper123 = {
   },
   tonconnect: {
     async connect(protocolVersion: number, message: ConnectRequest): Promise<ConnectEvent> {
-      console.log('connect', protocolVersion, message)
+      console.log('connect without ton_proof', protocolVersion, message)
       return {
         event: 'connect',
         id: 1716908283733,
@@ -152,21 +152,21 @@ const tonkeeper123 = {
                 'te6cckECFgEAAwQAAgE0AgEAUQAAAAApqaMXdgs06RoUlFDYAZyWDsCurtuEoQYYKXUSk5gwT4bK0fdAART/APSkE/S88sgLAwIBIAkEBPjygwjXGCDTH9Mf0x8C+CO78mTtRNDTH9Mf0//0BNFRQ7ryoVFRuvKiBfkBVBBk+RDyo/gAJKTIyx9SQMsfUjDL/1IQ9ADJ7VT4DwHTByHAAJ9sUZMg10qW0wfUAvsA6DDgIcAB4wAhwALjAAHAA5Ew4w0DpMjLHxLLH8v/CAcGBQAK9ADJ7VQAbIEBCNcY+gDTPzBSJIEBCPRZ8qeCEGRzdHJwdIAYyMsFywJQBc8WUAP6AhPLassfEss/yXP7AABwgQEI1xj6ANM/yFQgR4EBCPRR8qeCEG5vdGVwdIAYyMsFywJQBs8WUAT6AhTLahLLH8s/yXP7AAIAbtIH+gDU1CL5AAXIygcVy//J0Hd0gBjIywXLAiLPFlAF+gIUy2sSzMzJc/sAyEAUgQEI9FHypwICAUgTCgIBIAwLAFm9JCtvaiaECAoGuQ+gIYRw1AgIR6STfSmRDOaQPp/5g3gSgBt4EBSJhxWfMYQCASAODQARuMl+1E0NcLH4AgFYEg8CASAREAAZrx32omhAEGuQ64WPwAAZrc52omhAIGuQ64X/wAA9sp37UTQgQFA1yH0BDACyMoHy//J0AGBAQj0Cm+hMYALm0AHQ0wMhcbCSXwTgItdJwSCSXwTgAtMfIYIQcGx1Z70ighBkc3RyvbCSXwXgA/pAMCD6RAHIygfL/8nQ7UTQgQFA1yH0BDBcgQEI9ApvoTGzkl8H4AXTP8glghBwbHVnupI4MOMNA4IQZHN0crqSXwbjDRUUAIpQBIEBCPRZMO1E0IEBQNcgyAHPFvQAye1UAXKwjiOCEGRzdHKDHrFwgBhQBcsFUAPPFiP6AhPLassfyz/JgED7AJJfA+IAeAH6APQEMPgnbyIwUAqhIb7y4FCCEHBsdWeDHrFwgBhQBMsFJs8WWPoCGfQAy2kXyx9SYMs/IMmAQPsABntvxc8=',
               publicKey: '760b34e91a149450d8019c960ec0aeaedb84a106182975129398304f86cad1f7',
             },
-            {
-              name: 'ton_proof',
-              proof: {
-                // @ts-ignore
-                timestamp: 1716908279,
-                domain: {
-                  lengthBytes: 11,
-                  value: 'lucifer.ren',
-                },
-                signature:
-                  'r6AD3SdhXKCNhdSzJGNwbO2SBpyQ5P7s9Rl81undX+aLVgQNasKX+JtG7xFgPAsR3oizE0rw1JsDhNf8ipSyAQ==',
-                payload:
-                  'eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoiZWFhYjEwMDI5MzMxOTA2NmNhNjZjMzRkOWM5YmJhMjJjMjFmMzNkOGRkNWJhNzNiZTY5ZTgxNzQyZTRjYzlmZCIsImlhdCI6MTcxNjkwNzk4NiwiZXhwIjoxNzE2OTA4ODg2fQ.PW_SkU7GTsatpPsy0FGYzI4M_3NlSubyoILidektWO4',
-              },
-            },
+            // {
+            //   name: 'ton_proof',
+            //   proof: {
+            //     // @ts-ignore
+            //     timestamp: 1716908279,
+            //     domain: {
+            //       lengthBytes: 11,
+            //       value: 'lucifer.ren',
+            //     },
+            //     signature:
+            //       'r6AD3SdhXKCNhdSzJGNwbO2SBpyQ5P7s9Rl81undX+aLVgQNasKX+JtG7xFgPAsR3oizE0rw1JsDhNf8ipSyAQ==',
+            //     payload:
+            //       'eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoiZWFhYjEwMDI5MzMxOTA2NmNhNjZjMzRkOWM5YmJhMjJjMjFmMzNkOGRkNWJhNzNiZTY5ZTgxNzQyZTRjYzlmZCIsImlhdCI6MTcxNjkwNzk4NiwiZXhwIjoxNzE2OTA4ODg2fQ.PW_SkU7GTsatpPsy0FGYzI4M_3NlSubyoILidektWO4',
+            //   },
+            // },
           ],
           device: {
             platform: 'mac',
