2dad8c8d5121d6716d962da96453008dd72d7ec8
lucifer <azl397985856@gmail.com>
2024-06-12 06:23:15 +0000

    publish

--- src/server/services/ton-proof-service.ts
+++ src/server/services/ton-proof-service.ts
@@ -114,7 +114,7 @@ export class TonProofService {
       ]);
 
       const result = Buffer.from(await sha256(fullMsg));
-      console.log('checkProof wantedAddress gggg', result,  message.signature, publicKey)
+      console.log('checkProof wantedAddress gggg', fullMsg,  message.signature, publicKey)
       return sign.detached.verify(result, message.signature, publicKey);
     } catch (e) {
       return false;
