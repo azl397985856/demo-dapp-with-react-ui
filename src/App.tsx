c41db53006e4cc1bb781515f60fb600f40977d9d
lucifer <azl397985856@gmail.com>
2024-08-22 06:49:08 +0000

    publish

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,21 @@ function App() {
       manifestUrl="https://app.ston.fi/tonconnect-manifest.json"
       uiPreferences={{ theme: THEME.DARK }}
       walletsListConfiguration={{
-        includeWallets: [
-          {
-            appName: "nora",
-            name: "nora",
-            imageUrl: "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
-            tondns: "",
-            jsBridgeKey: "binancew3w",
-            aboutUrl: "https://www.safepal.com",
-            universalLink: "https://app.binance.com/cedefi/ton-connect",
-            // universalLink: 'bnc://app.binance.com/cedefi/ton-connect',
-            deepLink: 'bnc://app.binance.com/cedefi/ton-connect',
-            bridgeUrl: "https://bridge.tonapi.io/bridge",
-            platforms: ["chrome", "safari", "ios", "android"]
-          },
-        ]
+        // includeWallets: [
+        //   {
+        //     appName: "nora",
+        //     name: "nora",
+        //     imageUrl: "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
+        //     tondns: "",
+        //     jsBridgeKey: "binancew3w",
+        //     aboutUrl: "https://www.safepal.com",
+        //     universalLink: "https://app.binance.com/cedefi/ton-connect",
+        //     // universalLink: 'bnc://app.binance.com/cedefi/ton-connect',
+        //     deepLink: 'bnc://app.binance.com/cedefi/ton-connect',
+        //     bridgeUrl: "https://bridge.tonapi.io/bridge",
+        //     platforms: ["chrome", "safari", "ios", "android"]
+        //   },
+        // ]
       }}
       actionsConfiguration={{
         twaReturnUrl: 'https://t.me/DemoDappWithTonConnectBot/demo'
