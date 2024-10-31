95c1fc0d719148307f896174f7ec60c8d1c09b6b
lucifer <azl397985856@gmail.com>
2024-10-31 10:54:21 +0000

    publish

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <TonConnectUIProvider
-      manifestUrl="https://ton-connect.github1.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
+      manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
       uiPreferences={{ theme: THEME.DARK }}
       walletsListConfiguration={{
         // includeWallets: [
